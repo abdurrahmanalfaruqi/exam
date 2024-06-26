dda1a5ae9cb7abca53c41681bf70a9b7418ad5b5
alfaruq <54828722+abdurrahmanalfaruqi@users.noreply.github.com>
2024-06-26 07:25:57 +0700

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -26,4 +26,134 @@ class User {
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'emai
+      'email': email,
+      'first_name': firstName,
+      'last_name': lastName,
+      'avatar': avatar,
+    };
+  }
+}
+
+class Support {
+  String url;
+  String text;
+
+  Support({
+    required this.url,
+    required this.text,
+  });
+
+  factory Support.fromJson(Map<String, dynamic> json) {
+    return Support(
+      url: json['url'],
+      text: json['text'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'url': url,
+      'text': text,
+    };
+  }
+}
+
+class UserResponse {
+  int page;
+  int perPage;
+  int total;
+  int totalPages;
+  List<User> data;
+  Support support;
+
+  UserResponse({
+    required this.page,
+    required this.perPage,
+    required this.total,
+    required this.totalPages,
+    required this.data,
+    required this.support,
+  });
+
+  factory UserResponse.fromJson(Map<String, dynamic> json) {
+    return UserResponse(
+      page: json['page'],
+      perPage: json['per_page'],
+      total: json['total'],
+      totalPages: json['total_pages'],
+      data: List<User>.from(json['data'].map((user) => User.fromJson(user))),
+      support: Support.fromJson(json['support']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'page': page,
+      'per_page': perPage,
+      'total': total,
+      'total_pages': totalPages,
+      'data': data.map((user) => user.toJson()).toList(),
+      'support': support.toJson(),
+    };
+  }
+}
+
+void main() {
+  Map<String, dynamic> jsonData = {
+    "page": 2,
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2,
+    "data": [
+      {
+        "id": 7,
+        "email": "michael.lawson@reqres.in",
+        "first_name": "Michael",
+        "last_name": "Lawson",
+        "avatar": "https://reqres.in/img/faces/7-image.jpg"
+      },
+      {
+        "id": 8,
+        "email": "lindsay.ferguson@reqres.in",
+        "first_name": "Lindsay",
+        "last_name": "Ferguson",
+        "avatar": "https://reqres.in/img/faces/8-image.jpg"
+      },
+      {
+        "id": 9,
+        "email": "tobias.funke@reqres.in",
+        "first_name": "Tobias",
+        "last_name": "Funke",
+        "avatar": "https://reqres.in/img/faces/9-image.jpg"
+      },
+      {
+        "id": 10,
+        "email": "byron.fields@reqres.in",
+        "first_name": "Byron",
+        "last_name": "Fields",
+        "avatar": "https://reqres.in/img/faces/10-image.jpg"
+      },
+      {
+        "id": 11,
+        "email": "george.edwards@reqres.in",
+        "first_name": "George",
+        "last_name": "Edwards",
+        "avatar": "https://reqres.in/img/faces/11-image.jpg"
+      },
+      {
+        "id": 12,
+        "email": "rachel.howell@reqres.in",
+        "first_name": "Rachel",
+        "last_name": "Howell",
+        "avatar": "https://reqres.in/img/faces/12-image.jpg"
+      }
+    ],
+    "support": {
+      "url": "https://reqres.in/#support-heading",
+      "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+    }
+  };
+
+  UserResponse userResponse = UserResponse.fromJson(jsonData);
+  print(userResponse.toJson());
+}
